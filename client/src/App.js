1032d22fbffa27f4e771d6b569b4149a4844298c
luan <contact.henrique02@gmail.com>
2022-05-26 14:36:02 -0300

    back com front agora funcionando ( front ainda nao esilizado )

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header>
+     
      <h1>Alguma coisa</h1>
-     </header>
+    
+    
     </div>
   );
 }
