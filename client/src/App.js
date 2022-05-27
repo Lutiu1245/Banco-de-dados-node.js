5d94dde7533ab60dc127ed9bc4db212938480051
luan <contact.henrique02@gmail.com>
2022-05-27 09:27:35 -0300

    Não tenho tempo pra aprender pra consumir a api no front

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-
 import './App.css';
 
 
 
+
 function App() {
+  
   return (
     <div className="App">
-     
      <h1>Alguma coisa</h1>
-    
-    
     </div>
   );
 }
